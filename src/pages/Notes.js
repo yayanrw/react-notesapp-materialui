3603ed232692ebbafbdbeab544264c817d88fc9c
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-26 17:07:29 +0700

    mod notes to read from api

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import { responsiveFontSizes } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 
 function Notes() {
-    return (
-        <div>
-            Notes page
-        </div>
-    )
+  const [notes, setNotes] = useState([]);
+  const ApiUrl = "http://localhost:8000";
+
+  useEffect(() => {
+    fetch(`${ApiUrl}/notes`)
+      .then((res) => res.json())
+      .then((data) => setNotes(data))
+      .catch((e) => {});
+  }, []);
+
+  return (
+    <div>
+      {notes.map((note) => (
+        <p>{note.title}</p>
+      ))}
+    </div>
+  );
 }
 
-export default Notes
+export default Notes;
