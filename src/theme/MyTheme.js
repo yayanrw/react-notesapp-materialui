a90a6c35de11e4a22afcc9d33d7bcb5d46445c9e
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-26 15:36:02 +0700

    add mytheme

---
+++ src/theme/MyTheme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
+
+const myTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
+
+export default myTheme;
