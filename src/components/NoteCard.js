a001a3613dced6d873cdf81082d43e56ad4be05c
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-27 09:03:31 +0700

    mod card fix

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,26 +1,46 @@
-import {
-  Avatar,
-  Card,
-  CardContent,
-  CardHeader,
-  IconButton,
-  Typography,
-} from "@material-ui/core";
-import { DeleteOutline } from "@material-ui/icons";
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
+import { makeStyles } from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
+import { yellow, green, pink, blue } from "@material-ui/core/colors";
 
-function NoteCard({ note, handleDelete }) {
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: (note) => {
+      if (note.category === "work") {
+        return yellow[700];
+      }
+      if (note.category === "money") {
+        return green[500];
+      }
+      if (note.category === "todos") {
+        return pink[500];
+      }
+      return blue[500];
+    },
+  },
+});
+
+export default function NoteCard({ note, handleDelete }) {
+  const classes = useStyles(note);
+
   return (
     <div>
       <Card elevation={1}>
         <CardHeader
-          avatar={<Avatar aria-label="recipe"></Avatar>}
+          avatar={
+            <Avatar className={classes.avatar}>
+              {note.category[0].toUpperCase()}
+            </Avatar>
+          }
           action={
-            <IconButton
-              aria-label="delete"
-              onClick={() => handleDelete(note.id)}
-            >
-              <DeleteOutline />
+            <IconButton onClick={() => handleDelete(note.id)}>
+              <DeleteOutlined />
             </IconButton>
           }
           title={note.title}
@@ -35,5 +55,3 @@ function NoteCard({ note, handleDelete }) {
     </div>
   );
 }
-
-export default NoteCard;
