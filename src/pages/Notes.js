108f7fac21b796668fa1785d8ab19649c185da1c
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-27 07:50:50 +0700

    mod notes to handledlete

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,6 @@
+import { Container, Grid } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
+import NoteCard from "../components/NoteCard";
 import ApiUrl from "../helper/AppConfig";
 
 function Notes() {
@@ -11,12 +13,25 @@ function Notes() {
       .catch((e) => console.log(e));
   }, []);
 
+  const handleDelete = async (id) => {
+    await fetch(`${ApiUrl}/notes/${id}`, {
+      method: "delete",
+    });
+
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
   return (
-    <div>
-      {notes.map((note) => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
+    <Container>
+      <Grid container spacing={3}>
+        {notes.map((note) => (
+          <Grid item key={note.id} xs={12} md={6} lg={4}>
+            <NoteCard note={note} handleDelete={handleDelete} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
 
